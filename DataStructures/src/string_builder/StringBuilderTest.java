a6c63fadfdda137b8147d5b5e25c12293e380e0b
Adrien Tremblay <adrien@adrientremblay.com>
2021-01-12 20:31:38 -0500

    Create tests for StringBuilder

--- DataStructures/src/string_builder/StringBuilderTest.java
+++ DataStructures/src/string_builder/StringBuilderTest.java
@@ -3,12 +3,16 @@ package string_builder;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringBuilderTest {
+    private StringBuilder builder;
 
-    @org.junit.jupiter.api.Test
-    void append() {
+    @org.junit.jupiter.api.BeforeEach
+    void setup() {
+       builder = new StringBuilder();
     }
 
     @org.junit.jupiter.api.Test
-    void testToString() {
+    void append() {
+        builder.append("hello");
+        assertEquals("hello", builder.toString());
     }
 }
