3454147975246d7fa38c4d02de19ee370e080d2b
Adrien Tremblay <adrien@adrientremblay.com>
2021-05-25 11:02:42 -0400

    C# stuff

--- csharp/Tutorial/Tutorial/Tutorial/Program.cs
+++ csharp/Tutorial/Tutorial/Tutorial/Program.cs
@@ -7,8 +7,25 @@ using System.Threading.Tasks;
 namespace Tutorial {
 	class Program {
 		static void Main(string[] args) {
+			// print
 			Console.WriteLine("Hello World!");
+
+			// variables
+			int i = 56;
+			string meme = "hello" + " world";
+
+			// function calls
+			TestFunction();
+
 			Console.ReadKey();
+		}
+
+		static void TestFunction() {
+
+		}
+
+		static bool secondTestFunction(int i) {
+			return i > 100;
 		}
 	}
 }
