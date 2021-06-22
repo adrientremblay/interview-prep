70839d034f53e3b827af0456b4e19050402462d8
Adrien Tremblay <adrien.tremblay@mail.concordia.ca>
2021-06-22 13:44:51 -0400

    basic c++ project

---
+++ Algorithms(Python)/arrays/cmake-build-debug/CMakeFiles/arrays.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arrays.dir/meme.o"
+  "arrays.exe"
+  "arrays.pdb"
+  "libarrays.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arrays.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Algorithms(Python)/arrays/meme.cpp
@@ -0,0 +1,16 @@
+//
+// Created by atremblay on 6/22/2021.
+//
+
+#include <iostream>
+using namespace std;
+
+// Create a function
+void myFunction() {
+    cout << "I just got executed!";
+}
+
+int main() {
+    myFunction(); // call the function
+    return 0;
+}
